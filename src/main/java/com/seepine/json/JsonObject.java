00da5115e4cc62a432a01d7620cfc19060e2d65c
seepine <seepine@outlook.com>
2023-05-28 15:09:23 +0800

    fix(jsonObject): extend JsonNode

--- src/main/java/com/seepine/json/JsonObject.java
+++ src/main/java/com/seepine/json/JsonObject.java
@@ -1,24 +1,32 @@
 package com.seepine.json;
 
+import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.util.RawValue;
 import com.seepine.json.exception.JsonException;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * enhance objectNode get/set, the get will return null when there are no values or mismatched types
  *
  * @author seepine
  */
-public class JsonObject implements Serializable {
+public class JsonObject extends JsonNode implements Serializable {
   private static final long serialVersionUID = 1L;
-  ObjectNode objectNode;
+  private final ObjectNode objectNode;
 
   /** 构造空json对象 */
   public JsonObject() {
@@ -96,6 +104,7 @@ public class JsonObject implements Serializable {
    *
    * @return json串
    */
+  @Nonnull
   public String toJson() {
     return objectNode.toString();
   }
@@ -105,6 +114,7 @@ public class JsonObject implements Serializable {
    *
    * @return objectNode
    */
+  @Nonnull
   public ObjectNode toObjectNode() {
     return objectNode;
   }
@@ -114,6 +124,7 @@ public class JsonObject implements Serializable {
    *
    * @return jsonObject
    */
+  @Nonnull
   public JsonObject deepCopy() {
     return new JsonObject(objectNode.deepCopy());
   }
@@ -125,7 +136,8 @@ public class JsonObject implements Serializable {
    * @return T
    * @param <T> 类
    */
-  public <T> T toObject(Class<T> toValueType) {
+  @Nonnull
+  public <T> T toObject(@Nonnull Class<T> toValueType) {
     return Json.parse(objectNode, toValueType);
   }
 
@@ -134,16 +146,19 @@ public class JsonObject implements Serializable {
    *
    * @return Iterator<JsonNode>
    */
-  public Iterator<JsonNode> iterator() {
+  @Nonnull
+  public Iterator<JsonNode> iterators() {
     return objectNode.iterator();
   }
 
   /**
    * 构建JsonObject，等同 new JsonObject()
    *
+   * @code JsonObject.build()
    * @return jsonObject
    * @since 0.2.1
    */
+  @Nonnull
   public static JsonObject build() {
     return new JsonObject();
   }
@@ -151,9 +166,13 @@ public class JsonObject implements Serializable {
   /**
    * 构建JsonObject并设置值，等同 new JsonObject().set(field,v)
    *
+   * @code JsonObject.build("age",15)
+   * @param fieldName fieldName
+   * @param v value
    * @return jsonObject
    * @since 0.2.1
    */
+  @Nonnull
   public static JsonObject build(String fieldName, Object v) {
     return new JsonObject().set(fieldName, v);
   }
@@ -162,9 +181,10 @@ public class JsonObject implements Serializable {
    * 获取jsonNode
    *
    * @param fieldName 属性
-   * @return jsonNode
+   * @return jsonNode/null
    */
-  public JsonNode get(String fieldName) {
+  @Nullable
+  public JsonNode get(@Nonnull String fieldName) {
     return objectNode.get(fieldName);
   }
 
@@ -175,7 +195,8 @@ public class JsonObject implements Serializable {
    * @param value 值
    * @return jsonObject
    */
-  public JsonObject setIfAbsent(String fieldName, Object value) {
+  @Nonnull
+  public JsonObject setIfAbsent(@Nonnull String fieldName, @Nonnull Object value) {
     if (!has(fieldName)) {
       set(fieldName, value);
     }
@@ -189,7 +210,8 @@ public class JsonObject implements Serializable {
    * @param value 值
    * @return jsonObject
    */
-  public JsonObject set(String fieldName, Object value) {
+  @Nonnull
+  public JsonObject set(@Nonnull String fieldName, @Nullable Object value) {
     if (value == null) {
       objectNode.putNull(fieldName);
     } else if (value instanceof String) {
@@ -198,6 +220,8 @@ public class JsonObject implements Serializable {
       objectNode.put(fieldName, (Integer) value);
     } else if (value instanceof Boolean) {
       objectNode.put(fieldName, (Boolean) value);
+    } else if (value instanceof JsonObject) {
+      objectNode.set(fieldName, ((JsonObject) value).objectNode);
     } else if (value instanceof Double) {
       objectNode.put(fieldName, (Double) value);
     } else if (value instanceof Long) {
@@ -226,7 +250,7 @@ public class JsonObject implements Serializable {
    * @param fieldName 属性
    * @return 是否存在
    */
-  public boolean has(String fieldName) {
+  public boolean has(@Nonnull String fieldName) {
     return objectNode.has(fieldName);
   }
 
@@ -235,12 +259,18 @@ public class JsonObject implements Serializable {
    *
    * @param fieldName 属性
    * @return 数组/null
+   * @exception JsonException not array
    */
-  public ArrayNode getArray(String fieldName) {
-    if (!this.has(fieldName)) {
+  @Nullable
+  public ArrayNode getArray(@Nonnull String fieldName) throws JsonException {
+    JsonNode get = get(fieldName);
+    if (get == null) {
       return null;
     }
-    return get(fieldName).isArray() ? (ArrayNode) get(fieldName) : null;
+    if (!get.isArray()) {
+      throw new JsonException("'" + fieldName + "' not array");
+    }
+    return (ArrayNode) get(fieldName);
   }
 
   /**
@@ -248,24 +278,37 @@ public class JsonObject implements Serializable {
    *
    * @param fieldName 属性
    * @return 对象/null
+   * @exception JsonException not object
    */
-  public JsonObject getObj(String fieldName) {
-    if (!this.has(fieldName)) {
+  @Nullable
+  public JsonObject getObj(@Nonnull String fieldName) throws JsonException {
+    JsonNode get = get(fieldName);
+    if (get == null) {
       return null;
     }
-    return get(fieldName).isObject() ? new JsonObject(get(fieldName)) : null;
+    if (!get.isObject()) {
+      throw new JsonException("'" + fieldName + "' not object");
+    }
+    return new JsonObject(get(fieldName));
   }
   /**
    * 获取对象
    *
    * @param fieldName 属性
    * @return 对象/null
+   * @exception JsonException not object
    */
-  public <T> T getObject(String fieldName, Class<T> toValueType) {
-    if (!this.has(fieldName)) {
+  @Nullable
+  public <T> T getObject(@Nonnull String fieldName, @Nonnull Class<T> toValueType)
+      throws JsonException {
+    JsonNode get = get(fieldName);
+    if (get == null) {
       return null;
     }
-    return get(fieldName).isObject() ? Json.parse(get(fieldName), toValueType) : null;
+    if (!get.isObject()) {
+      throw new JsonException("'" + fieldName + "' not object");
+    }
+    return Json.parse(get(fieldName), toValueType);
   }
 
   /**
@@ -274,7 +317,8 @@ public class JsonObject implements Serializable {
    * @param fieldName 属性
    * @return 字符串/null
    */
-  public String getStr(String fieldName) {
+  @Nullable
+  public String getStr(@Nonnull String fieldName) {
     return this.has(fieldName) ? get(fieldName).asText() : null;
   }
 
@@ -284,7 +328,8 @@ public class JsonObject implements Serializable {
    * @param fieldName 属性
    * @return 整数/null
    */
-  public Integer getInt(String fieldName) {
+  @Nullable
+  public Integer getInt(@Nonnull String fieldName) {
     if (!this.has(fieldName)) {
       return null;
     }
@@ -304,7 +349,8 @@ public class JsonObject implements Serializable {
    * @param fieldName 属性
    * @return bool/null
    */
-  public Boolean getBool(String fieldName) {
+  @Nullable
+  public Boolean getBool(@Nonnull String fieldName) {
     if (!this.has(fieldName)) {
       return null;
     }
@@ -327,7 +373,8 @@ public class JsonObject implements Serializable {
    * @param fieldName 属性
    * @return long/null
    */
-  public Long getLong(String fieldName) {
+  @Nullable
+  public Long getLong(@Nonnull String fieldName) {
     if (!this.has(fieldName)) {
       return null;
     }
@@ -347,7 +394,8 @@ public class JsonObject implements Serializable {
    * @param fieldName 属性
    * @return BigDecimal/null
    */
-  public BigDecimal getBigDecimal(String fieldName) {
+  @Nullable
+  public BigDecimal getBigDecimal(@Nonnull String fieldName) {
     if (!this.has(fieldName)) {
       return null;
     }
@@ -366,12 +414,113 @@ public class JsonObject implements Serializable {
    * @param fieldName 属性
    * @return BigInteger/null
    */
-  public BigInteger getBigInteger(String fieldName) {
+  @Nullable
+  public BigInteger getBigInteger(@Nonnull String fieldName) {
     if (!this.has(fieldName)) {
       return null;
     }
     return get(fieldName).isBigInteger() || get(fieldName).isLong() || get(fieldName).isInt()
         ? new BigInteger(get(fieldName).asText())
         : null;
+  }
+
+  @Override
+  @Deprecated
+  public JsonNode get(int i) {
+    return objectNode.get(i);
+  }
+
+  @Override
+  @Deprecated
+  public JsonNode path(String s) {
+    return objectNode.path(s);
+  }
+
+  @Override
+  @Deprecated
+  public JsonNode path(int i) {
+    return objectNode.path(i);
+  }
+
+  @Override
+  protected JsonNode _at(JsonPointer jsonPointer) {
+    return this.get(jsonPointer.getMatchingProperty());
+  }
+
+  @Override
+  @Nonnull
+  public JsonNodeType getNodeType() {
+    return objectNode.getNodeType();
+  }
+
+  @Override
+  @Deprecated
+  public String asText() {
+    return objectNode.asText();
+  }
+
+  @Override
+  public JsonNode findValue(String s) {
+    return objectNode.findValue(s);
+  }
+
+  @Override
+  public JsonNode findPath(String s) {
+    return objectNode.findPath(s);
+  }
+
+  @Override
+  public JsonNode findParent(String s) {
+    return objectNode.findParent(s);
+  }
+
+  @Override
+  public List<JsonNode> findValues(String s, List<JsonNode> list) {
+    return objectNode.findValues(s, list);
+  }
+
+  @Override
+  public List<String> findValuesAsText(String s, List<String> list) {
+    return objectNode.findValuesAsText(s, list);
+  }
+
+  @Override
+  public List<JsonNode> findParents(String s, List<JsonNode> list) {
+    return objectNode.findParents(s, list);
+  }
+
+  @Override
+  public JsonToken asToken() {
+    return objectNode.asToken();
+  }
+
+  @Override
+  public JsonParser.NumberType numberType() {
+    return objectNode.numberType();
+  }
+
+  @Override
+  public JsonParser traverse() {
+    return objectNode.traverse();
+  }
+
+  @Override
+  public JsonParser traverse(ObjectCodec objectCodec) {
+    return objectNode.traverse(objectCodec);
+  }
+
+  @Override
+  public void serialize(JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+      throws IOException {
+    objectNode.serialize(jsonGenerator, serializerProvider);
+  }
+
+  @Override
+  public void serializeWithType(
+      JsonGenerator jsonGenerator,
+      SerializerProvider serializerProvider,
+      TypeSerializer typeSerializer)
+      throws IOException {
+    objectNode.serializeWithType(jsonGenerator, serializerProvider, typeSerializer);
   }
 }
